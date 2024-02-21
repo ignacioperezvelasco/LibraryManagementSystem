aeaf6f3e9cddaaf817389507ffbb21c4c670d5c9
Ignacio <ignacioperezvelasco12@gmail.com>
2024-02-21 17:36:46 +0100

    Starting client stuff

--- code/Classes/ClientManager/Cpp/client.cpp
+++ code/Classes/ClientManager/Cpp/client.cpp
@@ -1,1 +1,4 @@
 #include <Client.h>
+
+Client::Client(int _id, std::string _name): id(_id),name(_name)
+{}
--- code/Classes/ClientManager/Header/Client.h
+++ code/Classes/ClientManager/Header/Client.h
@@ -1,2 +1,13 @@
+#include <string>
+#include <memory>
+class Operations;
 class Client
-{};
+{
+    private:
+    int id;
+    std::string name;
+    int inCourseOpertion = 0;
+    int myOperations[5];
+    public:
+    Client(int id, std::string _name);
+};
